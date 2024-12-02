3261764e05b183655848659475c5c93d06237616
Kleber <lyuri1508@gmail.com>
2024-12-02 09:11:52 -0300

    Adicionado a funcionalidade de despesas e atualização do README

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# Budget Manager
+# Budget Manager V1.0
 
-Um gerenciador de orçamento simples em Python. Atualmente, ele permite:
+Um gerenciador de orçamento simples em Python que funciona inteiramente pelo terminal. Atualmente, ele permite:
 
 - Adicionar receitas.
 - Listar receitas.
 - Remover receitas.
 
+## Demonstração
+
+![Resultado da execução](images/execution_result.png)
 
 ## Próximos Passos
 
-- Implementar a funcionalidade de despesas.
-- Adicionar suporte a banco de dados.
-- Criar uma interface gráfica com Flet.
-
+- [x] Implementar a funcionalidade de despesas.
+- [ ] Adicionar suporte a banco de dados.
+- [ ] Criar uma interface gráfica com Flet.
--- app/main.py
+++ app/main.py
@@ -1,23 +1,34 @@
 from datetime import datetime
 
+total = 0
+
 # Lista para armazenar as receitas temporariamente
 receitas = []
 
+#Lista para armazenar as despesas temporariamente
+despesas = []
+
 def adicionar_receita(valor, categoria, data=None):
     """Função para adicionar uma nova receita."""
+    global total
+    
+    
     if data is None:
         data = datetime.now().strftime('%Y-%m-%d') #Data atual como padrão
     
     # Estrutura da receita usando um dicionário
     receita = {
-        "valor": valor,
+        "valor":  valor,
         "categoria": categoria,
         "data": data
     }
     
     # Adiciona a receita à lista de receitas
     receitas.append(receita)
+    total += receita["valor"]
     print("Receita adicionada com sucesso!")
+    print(f"Total: {total}")
+    
 
 def listar_receitas():
     print("\nLista de Receitas:")
@@ -38,8 +49,62 @@ def remover_receita():
     except ValueError:
         print("Entrada inválida. Por favor, digite um número.")
 
+
+def adicionar_despesa(valor, categoria, data=None):
+    global total
+    
+    if data is None:
+        data = datetime.now().strftime('%Y-%m-%d')
+    
+    despesa = {
+        "valor": valor,
+        "categoria": categoria,
+        "data": data
+    }
+    
+    if despesa["valor"] > total:
+        print(f"Infelizmente você não possuí saldo suficiente para adicionar está despesa: {despesa['categoria']}")
+        return
+    else:
+        despesas.append(despesa)
+        total -= despesa["valor"]
+        print(f"Despesa adicionada com sucesso!")
+
+
+def listar_despesas():
+    print("\nLista de despesas:")
+    for idx, despesa in enumerate(despesas, start=1):
+        print(f"{idx}. Valor: {despesa['valor']} | Categoria: {despesa['categoria']} | Data: {despesa['data']}")
+    print()
+
+def remover_despesa():
+    listar_despesas()
+    try:
+        index = int(input("Digite o número da receita que deseja remover: ")) - 1
+        if 0 <= index < len(despesas):
+            despesas.pop(index)
+            print("Despesa removida com sucesso.")
+            listar_despesas()
+        else:
+            print("Número inválido. Tente novamente.")
+    except ValueError:
+        print("Entrada inválida. Por favor, digite um número.")
+
+
 if __name__ == '__main__':
+    print(f'Receita total: {total}')
     adicionar_receita(500, "Salário")
     adicionar_receita(200, "Freelance", "2023-10-15")
     listar_receitas()
-    remover_receita()
+    
+    print("----------------------------------")
+    
+    adicionar_despesa(150, "Mercado")
+    adicionar_despesa(70, "Conta d'agua", "2023-11-07")
+    adicionar_despesa(700, "Parcela do Carro", "2023-11-21")
+    listar_despesas()
+    
+    print(f'Receita total: {total}')
+    
+    #remover_receita()
+    #remover_despesa()
